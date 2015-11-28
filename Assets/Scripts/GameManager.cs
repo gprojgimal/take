762a0208a9a3463f828e2866635d87caad0b8ad6
jinbaekl <jinb92@gmail.com>
2015-11-28 11:39:55 +0900

    중력 적용. Instantiate와 Destroy를 활용한 랜덤 생성 및 해제.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject goFalling;
+
+    private float _delayRegen = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(goFalling != null)
+        {
+            _delayRegen -= Time.deltaTime;
+            if(_delayRegen <= 0)
+            {
+                _delayRegen = 0.5f;
+                Instantiate(goFalling, new Vector3(Random.Range(-5.0f, 5.0f), 5.53f, 0), new Quaternion(0, 0, 0, 0));
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0, -0.1f));
+        if(transform.position.y < -3)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
