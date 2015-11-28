a09811a8263b0a8fa28f92d8e110ea5c1b6fb81e
jinbaekl <jinb92@gmail.com>
2015-11-28 11:16:47 +0900

    키보드 예제 제작. 샘플 2D 프로젝트 Asset Store에서 적용됨.

---
+++ Assets/Scripts/KeyboardExample.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardExample : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-0.2f, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(0.2f, 0, 0));
+        }
+        Debug.Log(transform.position);
+    }
+}
